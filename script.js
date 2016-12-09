5a6b7bbe0c048abe48d6dba01b9a5501a3db4e73
Ralph Minor <ralphminor@Ralphs-MacBook-Pro.local>
2016-12-09 11:17:16 -0700

    Added comment in main.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function (){
   var collection = [];
 })
 
+// Added this comment just to see a change in the branch on Github.
 
 $("#trigger").on("click", function(){
   console.log("button has been clicked !");
